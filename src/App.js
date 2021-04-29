4993a590caab3048518c5d118c21bf7853d55a38
joelsoedito <49315324+joelsoedito@users.noreply.github.com>
2021-04-29 12:52:10 +0200

    added internalization boilerplate for en, nl, it, fr, de
    Added internalization boilerplate with i18n. Add translations in the src/translations/languages files. LanguageChange is a temporary component to change language while running the app.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from "react-i18next";
+import React, { useState } from 'react';
+import { LanguageChangeText } from './LanguageChange';
 
 function App() {
+
+  // acces translations by declaring t
+  const { t } = useTranslation();
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{t("welcome")}</h1>
+      <LanguageChangeText />
     </div>
   );
 }
