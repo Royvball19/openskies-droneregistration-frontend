b1edd6e2cc60cb131d2b882e8b5b8f432111b5dc
Yannickvandenbos <74549055+Yannickvandenbos@users.noreply.github.com>
2021-05-05 12:11:03 +0200

    Added all endpoints in data class
    Added endpoints
    Added comments

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,28 +1,21 @@
-import React, {useEffect, useState} from 'react'
-import axios from "axios"
-import Data from '../service/Data'
+import React, { useEffect, useState } from "react";
+import Data from "../service/Data";
 
 function Test() {
-    const [operators, setOperators] = useState([])
+  const [operators, setOperators] = useState([]);
 
-    useEffect(() => {
-        Data.getAllOperators()
-        .then((response) => {
-          setOperators(response.data);
-          console.log(response);
-        }, [])
-        .catch((error) => {
-          console.log(error);
-        });
-    },[]
-    );
+  useEffect(() => {
+    Data.getAllOperators()
+      .then((response) => {
+        setOperators(response.data);
+        console.log(response);
+      }, [])
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-
-    return (
-        <div>
-          <h1>{operators[0].id}</h1>
-        </div>
-    )
+  return <div></div>;
 }
 
-export default Test
+export default Test;
