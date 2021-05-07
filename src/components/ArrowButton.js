827df230cc5d020a3db82a7304a8869dc25ccef3
Anne van der Aar <74538510+annevanderaar@users.noreply.github.com>
2021-05-07 13:59:03 +0100

    small fixes

--- src/components/ArrowButton.js
+++ src/components/ArrowButton.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+//Arrow button css made in menu.scss//
 import '../style/menu.css';
 import { MdKeyboardArrowRight } from "react-icons/md";
 
 export const ArrowButton = () => {
     
-
+    //Function to make the menu appear from the left side
     function openMenu() {
         document.getElementById("menuDiv").style.left = "0px";
         document.getElementById("buttonId").style.visibility = "hidden";
