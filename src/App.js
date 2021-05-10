052fad728f433f68bd9c6bf6bd9c056c71eb3f09
Royvball19 <royvball19@gmail.com>
2021-05-10 13:36:29 +0200

    Some fixes after merge

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import Data from "./service/Data";
 import Test from "./components/Test"
 import React, { useState } from "react";
+import ArrowButton from "./components/ArrowButton";
 import Menu from "./components/Menu";
 import TableView from "./components/Tables/TableView"
 import { LanguageChangeText } from "./components/LanguageChanger/LanguageChange";
@@ -9,7 +10,9 @@ import { LanguageChangeText } from "./components/LanguageChanger/LanguageChange"
 function App() {
   return (
     <div className="App">
-      <TableView tabletype="reports" />
+      <Menu />
+      <ArrowButton />
+      <TableView tabletype="operators" />
     </div>
   );
 }
