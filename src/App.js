b5c168b6dd4ff99cffc24e665af788d4c09aaa92
joelsoedito <49315324+joelsoedito@users.noreply.github.com>
2021-05-10 17:15:05 +0200

    added icon and styling for language menu

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import { useTranslation } from "react-i18next";
-import Data from "./service/Data";
-import Test from "./components/Test"
 import React, { useState } from "react";
-import Button from "./components/TestExample/TextExample"
-import Menu from "./components/Menu";
-import { LanguageChangeText } from "./components/LanguageChanger/LanguageChange";
+import { LanguageMenu } from "./components/LanguageChanger/LanguageMenu";
 import Search from "./components/Search"
 
 
 function App() {
 
-
   return (
     <div className="App">
+      <LanguageMenu className="language-menu"/>
       <Search type="drones" />
-      <LanguageChangeText />
       {/* <Test /> */}
     </div>
   );
