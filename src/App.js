27faaa45cc27854e2264151ed175f83a2a0bcc9b
annevanderaar <anne.vanderaar@student.hu.nl>
2021-05-11 13:24:45 +0200

    Moved filter in its own folder

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Test from "./components/Test"
 import React, { useState } from "react";
 import ArrowButton from "./components/ArrowButton";
 import Menu from "./components/Menu";
-import Filter from "./components/Filter";
+import Filter from "./components/Filter/Filter";
 import "./style/style.css";
 import TableView from "./components/Tables/TableView";
 import { LanguageChangeText } from "./components/LanguageChanger/LanguageChange";
