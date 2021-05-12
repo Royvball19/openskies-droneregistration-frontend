61649423984cca7073d7755af98c16d547ecbfc7
Royvball19 <royvball19@gmail.com>
2021-05-12 11:37:47 +0200

    Revert "Revert "Merge branch 'searchoverlay'""
    This reverts commit 8d66e15be8cf4b221bbbb498494cebaa196570bd.

--- src/translations/languages/nl.js
+++ src/translations/languages/nl.js
@@ -1,3 +1,8 @@
 export const TRANSLATIONS_NL = {
     welcome: "Welkom op de Applicatie",
+    searchHeader: "Alle",
+    searchFilter: "Filter",
+    searchSaveResult: "Opslaan",
+    searchShowAsChart: "Grafiek",
+    searchPlaceholder: "Zoeken op ",
 };
