e0458180d7bb12949cf8b8fdad1bb1f9f603774b
Yannickvandenbos <74549055+Yannickvandenbos@users.noreply.github.com>
2021-05-20 14:10:35 +0200

    FInished barChart

--- src/components/Charts/BarChart.js
+++ src/components/Charts/BarChart.js
@@ -3,75 +3,158 @@ import { ResponsiveBar } from "@nivo/bar";
 import "../../style/charts.css";
 import Data from "../../Data";
 
-function declareType(input) {
-  // declare tabletype and get route
-  switch (input) {
-    case "operators":
-      return Data.getAllOperators();
-    case "pilots":
-      return Data.getAllPilots();
-    case "aircrafts":
-      return Data.getAllAircrafts();
+function BarChart({}) {
+  let [dataSet, setData] = useState([]);
+  let [operatorData, setOperatorData] = useState({});
+  let [aircraftData, setAircraftData] = useState([]);
+  let [pilotData, setPilotData] = useState([]);
+  let [reportData, setReportData] = useState([]);
+  let [isLoaded, setLoaded] = useState(false);
+
+  function addTo(value) {
+    setData((prevArray) => [...prevArray, value]);
+    document.getElementById(value).style.display = "none";
+
+    if (value === "operators") {
+      document.getElementById("operatorsRemove").style.display = "block";
+    } else if (value === "pilots") {
+      document.getElementById("pilotsRemove").style.display = "block";
+    } else if (value === "aircrafts") {
+      document.getElementById("aircraftsRemove").style.display = "block";
+    } else if (value === "reports") {
+      document.getElementById("reportsRemove").style.display = "block";
+    }
   }
-}
 
-function BarChart({ type }) {
-  // set state
-  let [typeData, setTypeData] = useState([]);
+  function removeFrom(value) {
+    document.getElementById(value + "Remove").style.display = "none";
 
-  // fetch data from backend
+    let arrayCopy = dataSet;
+    let valueIndex = dataSet.indexOf(value);
+    arrayCopy.splice(valueIndex, 1);
+    setData(arrayCopy);
+
+    console.log(dataSet);
+    if (value === "operators") {
+      document.getElementById("operators").style.display = "block";
+    } else if (value === "pilots") {
+      document.getElementById("pilots").style.display = "block";
+    } else if (value === "aircrafts") {
+      document.getElementById("aircrafts").style.display = "block";
+    } else if (value === "reports") {
+      document.getElementById("reports").style.display = "block";
+    }
+  }
+
+  let data = [];
+
   useEffect(() => {
-    declareType(type)
+    Data.getAllOperators()
       .then((response) => {
-        setTypeData(response.data);
-        console.log(response.data);
+        setOperatorData(response.data.length);
       }, [])
       .catch((error) => {
         console.log(error);
       });
+    Data.getAllAircrafts()
+      .then((response) => {
+        setAircraftData(response.data.length);
+      }, [])
+      .catch((error) => {
+        console.log(error);
+      });
+    Data.getAllPilots()
+      .then((response) => {
+        setPilotData(response.data.length);
+      }, [])
+      .catch((error) => {
+        console.log(error);
+      });
+    Data.getAllReports()
+      .then((response) => {
+        setReportData(response.data.length);
+      }, [])
+      .catch((error) => {
+        console.log(error);
+      });
+    setLoaded(true);
   }, []);
 
-  let lenght = [];
-
-  if (!typeData.length) {
-    return null;
-  } else {
-    lenght = [
+  if (isLoaded) {
+    let data = [
       {
-        amount: typeData.length,
+        object: "operators",
+        operators: operatorData,
+      },
+      {
+        object: "pilots",
+        pilots: pilotData,
+      },
+      {
+        object: "aircrafts",
+        aircrafts: aircraftData,
+      },
+      {
+        object: "reports",
+        reports: reportData,
       },
     ];
+
     return (
       <div className="chart-parent">
+        <div className="addButtons">
+          <button id="operators" onClick={() => addTo("operators")}>
+            operators +
+          </button>
+          <button id="pilots" onClick={() => addTo("pilots")}>
+            pilots +
+          </button>
+          <button id="aircrafts" onClick={() => addTo("aircrafts")}>
+            aircrafts +
+          </button>
+          <button id="reports" onClick={() => addTo("reports")}>
+            reports +
+          </button>
+        </div>
+        <div>
+          <button
+            className="removeButtons"
+            id="operatorsRemove"
+            onClick={() => removeFrom("operators")}
+          >
+            operators -
+          </button>
+          <button
+            className="removeButtons"
+            id="pilotsRemove"
+            onClick={() => removeFrom("pilots")}
+          >
+            pilots -
+          </button>
+          <button
+            className="removeButtons"
+            id="aircraftsRemove"
+            onClick={() => removeFrom("aircrafts")}
+          >
+            aircrafts -
+          </button>
+          <button
+            className="removeButtons"
+            id="reportsRemove"
+            onClick={() => removeFrom("reports")}
+          >
+            reports -
+          </button>
+        </div>
         <ResponsiveBar
-          data={lenght}
-          keys={["amount"]}
-          indexBy={type}
+          data={data}
+          keys={dataSet}
+          indexBy="object"
           margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
           padding={0.3}
           valueScale={{ type: "linear" }}
           indexScale={{ type: "band", round: true }}
-          colors={{ scheme: "blues" }}
-          defs={[
-            {
-              id: "dots",
-              type: "patternDots",
-              background: "inherit",
-              color: "#38bcb2",
-              size: 4,
-              padding: 1,
-              stagger: true,
-            },
-            {
-              id: "lines",
-              type: "patternLines",
-              background: "inherit",
-              color: "#eed312",
-              rotation: -45,
-              lineWidth: 6,
-              spacing: 10,
-            },
-          ]}
+          colors={{ scheme: "nivo" }}
           borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
           axisTop={null}
           axisRight={null}
@@ -79,7 +162,7 @@ function BarChart({ type }) {
             tickSize: 5,
             tickPadding: 5,
             tickRotation: 0,
-            legend: "amount of " + type,
+            legend: "",
             legendPosition: "middle",
             legendOffset: 32,
           }}
@@ -87,7 +170,7 @@ function BarChart({ type }) {
             tickSize: 5,
             tickPadding: 5,
             tickRotation: 0,
-            legend: "registries",
+            legend: "number",
             legendPosition: "middle",
             legendOffset: -40,
           }}
@@ -124,6 +207,8 @@ function BarChart({ type }) {
         />
       </div>
     );
+  } else {
+    return null;
   }
 }
 
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,13 @@
 import React from "react";
-import ChartsMenu from "./ChartsMenu";
 import "../../style/dashboard.css";
-import { RiLineChartFill } from "react-icons/ri";
+import BarChart from "../Charts/BarChart";
 
 export default function Dashboard() {
   return (
     <div>
       <div className="dashboard-container">
         <h1 className="dashboard-title">Dashboard</h1>
-        <ChartsMenu />
+        <BarChart />
       </div>
     </div>
   );
