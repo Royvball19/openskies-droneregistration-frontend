b4db1b9a123f1adaa5cead305cf9eacb989f0588
Royvball19 <royvball19@gmail.com>
2021-05-25 11:05:29 +0200

    Fixed charts

--- src/components/Charts/BarChart.js
+++ src/components/Charts/BarChart.js
@@ -8,7 +8,7 @@ function BarChart({ dataSet, data }) {
     <div id="chart-parent" className="chart-parent">
       <ResponsiveBar
         data={data}
-        keys={dataSet}
+        keys={["operators", "pilots", "aircrafts", "reports"]}
         indexBy="object"
         margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
         padding={0.3}
--- src/components/Charts/Charts.js
+++ src/components/Charts/Charts.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { ResponsiveBar } from "@nivo/bar";
 import BarChart from "./BarChart";
 import PieChart from "./PieChart";
 import LineChart from "./LineChart";
@@ -7,46 +6,11 @@ import "../../style/charts.css";
 import Data from "../../Data";
 
 function Charts({}) {
-  let [dataSet, setData] = useState([]);
   let [operatorData, setOperatorData] = useState({});
   let [aircraftData, setAircraftData] = useState([]);
   let [pilotData, setPilotData] = useState([]);
   let [reportData, setReportData] = useState([]);
   let [isLoaded, setLoaded] = useState(false);
-
-  function addTo(value) {
-    setData((prevArray) => [...prevArray, value]);
-    document.getElementById(value).style.display = "none";
-
-    if (value === "operators") {
-      document.getElementById("operatorsRemove").style.display = "block";
-    } else if (value === "pilots") {
-      document.getElementById("pilotsRemove").style.display = "block";
-    } else if (value === "aircrafts") {
-      document.getElementById("aircraftsRemove").style.display = "block";
-    } else if (value === "reports") {
-      document.getElementById("reportsRemove").style.display = "block";
-    }
-  }
-
-  function removeFrom(value) {
-    document.getElementById(value + "Remove").style.display = "none";
-    let arrayCopy = dataSet;
-    let valueIndex = dataSet.indexOf(value);
-    arrayCopy.splice(valueIndex, 1);
-    setData(arrayCopy);
-
-    console.log(dataSet);
-    if (value === "operators") {
-      document.getElementById("operators").style.display = "block";
-    } else if (value === "pilots") {
-      document.getElementById("pilots").style.display = "block";
-    } else if (value === "aircrafts") {
-      document.getElementById("aircrafts").style.display = "block";
-    } else if (value === "reports") {
-      document.getElementById("reports").style.display = "block";
-    }
-  }
 
   useEffect(() => {
     Data.getAllOperators()
@@ -110,53 +74,9 @@ function Charts({}) {
 
     return (
       <div className="charts">
-        <BarChart dataSet={dataSet} data={data} />
-        <PieChart dataSet={dataSet} data={data} />
-        <LineChart dataSet={dataSet} data={data} />
-        <div className="addButtons">
-          <button id="operators" onClick={() => addTo("operators")}>
-            operators +
-          </button>
-          <button id="pilots" onClick={() => addTo("pilots")}>
-            pilots +
-          </button>
-          <button id="aircrafts" onClick={() => addTo("aircrafts")}>
-            aircrafts +
-          </button>
-          <button id="reports" onClick={() => addTo("reports")}>
-            reports +
-          </button>
-        </div>
-        <div>
-          <button
-            className="removeButtons"
-            id="operatorsRemove"
-            onClick={() => removeFrom("operators")}
-          >
-            operators -
-          </button>
-          <button
-            className="removeButtons"
-            id="pilotsRemove"
-            onClick={() => removeFrom("pilots")}
-          >
-            pilots -
-          </button>
-          <button
-            className="removeButtons"
-            id="aircraftsRemove"
-            onClick={() => removeFrom("aircrafts")}
-          >
-            aircrafts -
-          </button>
-          <button
-            className="removeButtons"
-            id="reportsRemove"
-            onClick={() => removeFrom("reports")}
-          >
-            reports -
-          </button>
-        </div>
+        <BarChart data={data} />
+        <PieChart data={data} />
+        <LineChart data={data} />
       </div>
     );
   } else {
