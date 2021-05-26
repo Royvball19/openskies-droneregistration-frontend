834bfccad4e3aa622ade094649b79d77002bc9c1
Royvball19 <royvball19@gmail.com>
2021-05-26 10:09:26 +0200

    First version of charts

--- src/components/Tables/TableView.js
+++ src/components/Tables/TableView.js
@@ -3,13 +3,33 @@ import React, { useEffect, useState } from "react";
 import BootstrapTable from "react-bootstrap-table-next";
 import TableTypes from "../../DataTypes";
 import "../../style/tableview.css";
+import moment from "moment";
 
-export default function TableView( { data, columns}) {
-  
-  if (columns === null) {
+export default function TableView({ data, columns }) {
+  if (!data.length) {
     return null;
-
   } else {
+    // const test2 = data[0].created_at;
+
+    // console.log(moment(test2).calendar());
+    console.log(data);
+
+    // let newDate = data.map(
+    //   (datas) =>
+    //     (data = {
+    //       id: datas.id,
+    //       registration_mark: datas.registration_mark,
+    //       maci_number: datas.maci_number,
+    //       mass: datas.mass,
+    //       model: datas.model,
+    //       status: datas.status,
+    //       created_at: moment(datas.created_at).calendar(),
+    //       updated_at: moment(datas.updated_at).calendar(),
+    //     })
+    // );
+
+    // console.log(newDate);
+
     return (
       <div className="tableview">
         <div className="table">
